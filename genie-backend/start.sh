c3931cc66bcf615fb2e4c8a2e10998f895a9fb26
Miraclewang <549788813@qq.com>
2025-07-24 10:34:28 +0800

    Update start.sh
    fix: typo
    
    Signed-off-by: Miraclewang <549788813@qq.com>

--- genie-backend/start.sh
+++ genie-backend/start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # 开始启动后端程序
-BASEDIR="./genie-backend"
+BASEDIR="./target/genie-backend"
 CLASSPATH="$BASEDIR/conf/:$BASEDIR/lib/*"
 MAIN_MODULE="com.jd.genie.GenieApplication"
 LOGFILE="./genie-backend_startup.log"
-
-sed -i "s#<input llm server here>#${OPENAI_BASE_URL}#g" "$BASEDIR/conf/application.yml"
-sed -i "s/<input llm key here>/$OPENAI_API_KEY/g" $BASEDIR/conf/application.yml
 
 echo "starting $APP_NAME :)"
 java -classpath "$CLASSPATH" -Dbasedir="$BASEDIR" -Dfile.encoding="UTF-8" ${MAIN_MODULE} > $LOGFILE 2>&1 &
